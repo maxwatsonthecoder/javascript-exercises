5c5787f76a395762fa93655ec06bd92aba569eba
Max <140012390+maxwatsonthecoder@users.noreply.github.com>
2023-10-17 18:30:13 -0500

    Complete modules 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
 
-};
+
+    let repeatedString = '';
+    while (times > 0) {
+        repeatedString += string;
+        times--;
+    }
+    return repeatedString;
+}
+
 
 // Do not edit below this line
 module.exports = repeatString;
